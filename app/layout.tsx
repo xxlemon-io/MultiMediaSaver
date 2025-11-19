4eac8fad20ced5225eed505c9997853d716f8d42
XXNM <ame9807@outlook.com>
2025-11-20 02:46:57 +0900

    Add SVG icon and update layout metadata to include icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "MultiMediaSaver - Media Downloader",
   description: "Download images and videos from Twitter/X and Instagram",
+  icons: {
+    icon: "/icon.svg",
+  },
 };
 
 export default function RootLayout({
